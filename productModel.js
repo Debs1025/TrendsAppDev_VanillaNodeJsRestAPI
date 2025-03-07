d54dd98128b25afa3a2ef843b0fb96b91115c013
Erick De Belen <181858092+Debs1025@users.noreply.github.com>
2025-03-07 23:58:59 +0800

    Add files via upload

---
+++ productModel.js
@@ -0,0 +1,65 @@
+let products = require('../data/products')
+
+const { writeDataToFile } = require('../utils')
+
+function generateRandomId() {
+   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   let result = '';
+   for (let i = 0; i < 10; i++) {
+       result += characters.charAt(Math.floor(Math.random() * characters.length));
+   }
+   return result;
+}
+
+function findAll() {
+    return new Promise((resolve, reject) => {
+        resolve(products)
+    })
+}
+
+function findById(id) {
+    return new Promise((resolve, reject) => {
+        const product = products.find((p) => p.id === id)
+        resolve(product)
+    })
+}
+
+function create(product) {
+    return new Promise((resolve, reject) => {
+        const newProduct = {id: generateRandomId(), ...product}
+        products.push(newProduct)
+        if (process.env.NODE_ENV !== 'test') {
+            writeDataToFile('./data/products.json', products);
+        }
+        resolve(newProduct)
+    })
+}
+
+function update(id, product) {
+    return new Promise((resolve, reject) => {
+        const index = products.findIndex((p) => p.id === id)
+        products[index] = {id, ...product}
+        if (process.env.NODE_ENV !== 'test') {
+            writeDataToFile('./data/products.json', products);
+        }
+        resolve(products[index])
+    })
+}
+
+function remove(id) {
+    return new Promise((resolve, reject) => {
+        products = products.filter((p) => p.id !== id)
+        if (process.env.NODE_ENV !== 'test') {
+            writeDataToFile('./data/products.json', products);
+        }
+        resolve()
+    })
+}
+
+module.exports = {
+    findAll,
+    findById,
+    create,
+    update,
+    remove
+}
